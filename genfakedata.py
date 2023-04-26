bbfa875d5e2c3a6ac055740988ce1f4da57ad371
ch-marv <131853930+ch-marv@users.noreply.github.com>
2023-04-26 01:45:21 -0600

    Add files via upload

---
+++ genfakedata.py
@@ -0,0 +1,54 @@
+import numpy as np
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+from meteostat import Point, Daily
+from meteostat import Hourly
+# Import Meteostat library
+from meteostat import Stations
+import pandas
+import random
+
+def fetch():
+    # Get nearby weather stations
+    stations = Stations()
+    stations = stations.nearby(45.6815, -111.0320)
+    station = stations.fetch(3)
+    start = datetime(2022,1,1)
+    end = datetime(2022,12,31)
+    data = Hourly('KEKS0', start, end)
+    data = data.fetch()
+    print(data)
+    # Print DataFrame
+    data.to_csv('fake_data3.csv')
+
+#KBZN0
+#KLVM0
+#KEKS0
+
+#with open('fakedata.csv', 'w') as file:
+#    file.close()
+
+def genfake():
+    #offsets
+        #direct offsets
+        #date/time based offsets
+        #add multiploer
+        #general noise
+    df = pandas.read_csv("sc1.csv")
+    #df.loc[row_indexer,column_indexer]
+    templist = []
+    for i in range():
+       temp = float(df.loc[i,"temp"])
+       temp = temp + random.uniform(0.5,-0.5)
+       temp = temp + 3.5
+       templist.append(round(temp,1))
+    print(templist)
+
+
+       
+# AI will predict the weather in the following hours, maybe hour 1, 2, 3, 4, 5, 6
+# need to check these values at a fake(measured) data index, match that index to historical data and get
+# hours 1 - 6 from historical data
+
+genfake()
---
+++ main.py
@@ -0,0 +1,20 @@
+#Inputs
+#Date
+#Temp Source 1
+#Temp Source 2
+#Temp Source 3
+#Humi Source 1
+#Humi Source 2
+#Humi Source 3
+#Press Source 1
+#Press Source 1
+#Press Source 2
+#Wind Source 3
+#Wind Source 2
+#Wind Source 3
+#Precip Source 1
+#Precip Source 2
+#Precip Source 3
+#C C Source 1
+#C C Source 2
+#C C Source 3
